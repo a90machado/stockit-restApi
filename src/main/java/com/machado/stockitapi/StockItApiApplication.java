910a0f09c2fb4a597993ce8d1ac98597fa0ba24c
Machado <a90machado@gmail.com>
2020-09-28 12:41:09 +0100

    initial commit

---
+++ src/main/java/com/machado/stockitapi/StockItApiApplication.java
@@ -0,0 +1,13 @@
+package com.machado.stockitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockItApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockItApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/machado/stockitapi/StockItApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machado.stockitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockItApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
